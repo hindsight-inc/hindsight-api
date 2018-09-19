d466534c005934827fe2885b2463db3bad100482
Leo Liu <yliu@almondbank.com>
2018-09-18 21:32:35 -0400

    Added package error

--- user/request.go
+++ user/request.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"hindsight/database"
+	"hindsight/error"
 )
 
 func (self *User) Response() gin.H {
@@ -16,19 +17,21 @@ curl -v -X POST \
   -H 'content-type: application/json' \
   -d '{ "username": "username001", "password": "password001" }'
 */
-func UserRegister(context *gin.Context) {
+func UserRegister(c *gin.Context) {
 	db := database.GetDB()
 	var user User
-	if err := context.ShouldBindJSON(&user); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	db.Where(User{Username: user.Username}).First(&user)
 	if user.ID == 0 {
 		db.Create(&user)
-		context.JSON(http.StatusOK, user.Response())
+		c.JSON(http.StatusOK, user.Response())
 	} else {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		//c.JSON(http.StatusBadRequest, error.New("User already exists"))
+		//c.JSON(error.New("user.register.existing"))
+		c.JSON(error.H(error.UserRegisterExisting))
 	}
 }
 
@@ -38,20 +41,20 @@ curl -v -X POST \
   -H 'content-type: application/json' \
   -d '{ "username": "username001", "password": "password001" }'
 */
-func UserLogin(context *gin.Context) {
+func UserLogin(c *gin.Context) {
 	var user User
-	if err := context.ShouldBindJSON(&user); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	if user.Username != "username001" || user.Password != "password001" {
-		context.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"status": "success"})
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
-func UserInfo(context *gin.Context) {
+func UserInfo(c *gin.Context) {
 	user := User{Username: "username002"}
-	context.JSON(200, user.Response())
+	c.JSON(200, user.Response())
 }
