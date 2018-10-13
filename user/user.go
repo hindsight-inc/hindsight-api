d2e8f25406c436c4dab6af27c3512bd4912e4ebf
Leo Liu <yliu@almondbank.com>
2018-10-12 23:30:01 -0400

    Code cleanup

--- user/user.go
+++ user/user.go
@@ -16,28 +16,17 @@ type User struct {
 //	Don't use old token after changing this, see: https://github.com/appleboy/gin-jwt/issues/170
 const IdentityKey = "user.id"
 
-/*
-func New(username string) *User {
-	return &User{Username: username}
-}
-*/
-
 func Current(c *gin.Context) *User {
 	// TODO: performance issue - when topic.Create is called, firstly Authenticate checks if user is valid, then Current is called to obtain the user. Can we combine these 2 queries by getting user.ID from JWT?
 	var u User
 	claim := jwt.ExtractClaims(c)[IdentityKey]
 	if claim == nil {
-		//c.JSON(error.Unauthorized(error.DomainUserInfo, error.ReasonEmptyEntry, "Missing authorization info"))
 		return nil
 	}
 	username := claim.(string)
-	//u, _ := c.Get(IdentityKey)
-	//username := u.(*User).Username
 	db := database.GetDB()
 	db.Where(User{Username: username}).First(&u)
 	if u.ID == 0 {
-		//	Shouldn't reach here unless user has been deleted but active token is not
-		//c.JSON(error.Bad(error.DomainUserInfo, error.ReasonNonexistentEntry, "User not found"))
 		return nil
 	}
 	return &u
