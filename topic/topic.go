d2c274052095ebf308c625a153da3696ac13ca78
Leo Liu <yliu@almondbank.com>
2018-11-12 01:36:31 -0500

    Refactoring: created topic/opinion.go

--- topic/topic.go
+++ topic/topic.go
@@ -81,44 +81,6 @@ func (self *Topic) DetailResponse() (int, gin.H) {
 	}
 }
 
-/* Opinion */
-
-type Opinion struct {
-	gorm.Model
-	Title		string
-	TopicID		uint
-	Author		user.User
-	AuthorID	uint
-}
-
-type tOpinions []Opinion
-
-/* Request */
-
-type OpinionRequest struct {
-	Title		string
-}
-
-/* Response */
-
-func (self *Opinion) Response() gin.H {
-	return gin.H{"title": self.Title}
-}
-
-func (self *tOpinions) Response() []gin.H {
-	var opinions []gin.H
-	for _, opinion := range *self {
-		opinions = append(opinions, opinion.Response())
-	}
-	return opinions
-}
-
-/*
-type OpinionResponse struct {
-	Title		string
-}
-*/
-
 /* Vote */
 
 type Vote struct {
