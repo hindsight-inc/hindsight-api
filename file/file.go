8fbcfae82e06fbb60e0b9b23ca8f7fd787ef5fbe
Leo Liu <yliu@almondbank.com>
2018-11-15 22:19:42 -0500

    Added image upload API: file/image (in progress)

--- file/file.go
+++ file/file.go
@@ -1,4 +1,4 @@
-package main
+package file
 
 import (
 	"fmt"
@@ -6,7 +6,44 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"github.com/jinzhu/gorm"
 )
+
+type Image struct {
+	gorm.Model
+	Filename string
+	ContentType string
+	Title string
+	UID string
+}
+
+func UploadImage(c *gin.Context) {
+	title := c.PostForm("title")
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+		return
+	}
+
+	mime := file.Header.Get("Content-Type")
+	if !strings.Contains(mime, "image") {
+		c.String(http.StatusBadRequest, fmt.Sprintf("not image: %s", mime))
+		return
+	}
+
+	x := xid.New()
+	//	TODO: check if file is image
+	//	TODO: check file size
+
+	if err := c.SaveUploadedFile(file, "./public/upload/image/" + file.Filename); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+		return
+	}
+
+	c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s. type: %s", file.Filename, x, title, file.Header.Get("Content-Type")))
+}
 
 func main() {
 	router := gin.Default()
