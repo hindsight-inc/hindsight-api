fd7e7d604d3fdc76b3647705d09c130bd1cf98a9
Leo Liu <yliu@almondbank.com>
2018-11-15 21:19:07 -0500

    Added image type checking

--- image/image.go
+++ image/image.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,12 +24,19 @@ func main() {
 			return
 		}
 
+		mime := file.Header.Get("Content-Type")
+		if !strings.Contains(mime, "image") {
+			c.String(http.StatusBadRequest, fmt.Sprintf("not image: %s", mime))
+			return
+		}
+		//	TODO: check if file is image
+		//	TODO: check file size
 		if err := c.SaveUploadedFile(file, "../public/upload/image/" + file.Filename); err != nil {
 			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 			return
 		}
 
-		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email))
+		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s. type: %s", file.Filename, name, email, file.Header.Get("Content-Type")))
 	})
 	router.Run(":8080")
 }
